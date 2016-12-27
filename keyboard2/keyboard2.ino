90834e95bd6031d4e89282efd0c1b1e58ec5a2fd
nminer <you@example.com>
2016-12-26 20:53:53 -0500

    added a debounce for key press and release.

--- keyboard2/keyboard2.ino
+++ keyboard2/keyboard2.ino
@@ -1,13 +1,15 @@
-// array for the pins for all the comlumns.
-int Column[] = {12, 11, 10, 9, 8, 7, 6, 5, 2, 23, 14, 15, 16, 4 };
-// array for the pins for all the rows.
-int Row[] = {18, 19, 20, 21, 22};
+// Array for the pins for all the comlumns.
+int Column[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+// Array for the pins for all the rows.
+int Row[] = {16, 17, 18, 19, 20};
 
-bool functionPressed = false;     // set to true if function key is pressed.
-bool pressedKeys[70];             // keep track of what keys are pressed.
-int pressedCount = 0;             // keep track of how many keys are pressed.
+int maxKeysPressed = 6; // Set to the maximum number of keys that can be pressed at a time.
 
-// all the key when function key is not pressed.
+// Sets number of times to loop and read a pin to decreases the number of false positives. 
+int keyPressDebounce = 1; // I could not set this to high or it would always read false at some point.
+int keyReleaseDebounce = 2;
+
+// All the key when function key is not pressed.
 int keys[] = {'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', KEY_BACKSPACE,
               KEY_TAB, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
               KEY_CAPS_LOCK, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'J', KEY_RETURN,
@@ -15,19 +17,21 @@ int keys[] = {'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', K
               KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, 'B', 'C', 'D', ' ', 'E', 'G', 'H', KEY_RIGHT_ALT, 'F', KEY_RIGHT_GUI, KEY_LEFT_CTRL
              };
 
-// array of all the keys when function key has been pressed.
+// Array of all the keys when function key has been pressed.
 int funkeys[] = {KEY_F1, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7, KEY_F8, KEY_F9, KEY_F10, KEY_F11, KEY_F12, KEY_DELETE,
                  KEY_TAB, 'q', KEY_UP_ARROW, 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
                  KEY_CAPS_LOCK, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', ' ', KEY_RETURN,
                  KEY_LEFT_SHIFT, ' ', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', ' ', KEY_RIGHT_SHIFT,
                  KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, ' ', ' ', ' ', ' ', ' ', ' ', ' ', KEY_RIGHT_ALT, 'F', KEY_RIGHT_GUI, KEY_LEFT_CTRL
                 };
+                
+bool pressedKeys[70];             // Keep track of what keys are p`11-22ressed.
+int pressedCount = 0;             // Keep track of how many keys are pressed.
 
-//array that points to the function keys
-//(this should stary the same on both the keys[] and the funkeys marked with F char.
+// Array that points to the function keys
+// (this should stary the same on both the keys[] and the funkeys marked with F char.
 int functionKey[] = {67};
-int numverOfFunKeys = 1; // set to the size of the array does not like the sizeof.
-bool funcKeyPressed = false; // set to true buy function checkFunctionKeyPressed if the F key is pressed.
+int numverOfFunKeys = 1; // Set to the size of the array does not like the sizeof.
 
 /* Set all the pins for input andout put. */
 void setup() {
@@ -53,7 +57,7 @@ void loop() {
     //delay(10);
     for (int r = 0; r < 5 ; r++) {
       //read each row for a key press.
-      if (digitalRead(Row[r]) == HIGH) {
+      if (digitalRead(Row[r]) == HIGH && debouncePress(r)) {
         checkPressKey(r, c);
       } else {
         checkReleaseKey(r, c);
@@ -65,15 +69,14 @@ void loop() {
 
 /* presses the key for the given row and column */
 void checkPressKey(int r, int c) {
-  if (pressedCount < 6 && pressedKeys[r * 14 + c] != true) {
+  if (pressedCount < maxKeysPressed && pressedKeys[r * 14 + c] != true) {
     if (keys[r * 14 + c] == 'F') { // if the key being pressed is a function key
       Keyboard.releaseAll();
       clearAllPressed();
       pressedCount = 0;
       addKey(r, c);
     } else {
-      checkFunctionKeyPressed();
-      if (funcKeyPressed) {
+      if (checkFunctionKeyPressed()) {
         Keyboard.press(funkeys[r * 14 + c]);
       } else {
         Keyboard.press(keys[r * 14 + c]);
@@ -85,15 +88,14 @@ void checkPressKey(int r, int c) {
 
 /* Releases the key for the given row and column */
 void checkReleaseKey(int r, int c) {
-  if (pressedKeys[r * 14 + c] == true) {
+  if (pressedKeys[r * 14 + c] == true && debounceRelease(r)) {
     if (keys[r * 14 + c] == 'F') { // if the key being released is a function key
       Keyboard.releaseAll();
       clearAllPressed();
       pressedCount = 0;
           removeKey(r, c);
     } else {
-      checkFunctionKeyPressed();
-      if (funcKeyPressed) {
+      if (checkFunctionKeyPressed()) {
         Keyboard.release(funkeys[r * 14 + c]);
       } else {
         Keyboard.release(keys[r * 14 + c]);
@@ -101,6 +103,32 @@ void checkReleaseKey(int r, int c) {
       removeKey(r, c);
     }
   }
+}
+
+/*  return true if for all the loops it reads high.
+ *  The numbers of loops is determined by
+ *  keyPressDebounce.
+ */
+bool debouncePress(int r) {
+  for(int i = 0; i < keyPressDebounce; i++) {
+    if (digitalRead(Row[r]) != HIGH) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/*  return true if for all the loops it reads low.
+ *  The numbers of loops is determined by
+ *  keyReleaseDebounce.
+ */
+bool debounceRelease(int r) {
+  for(int i = 0; i < keyReleaseDebounce; i++) {
+    if (digitalRead(Row[r]) != LOW) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* set the givin key to true in the pressed array. pressedKeys[] */
@@ -119,13 +147,13 @@ void removeKey(int r, int c) {
 }
 
 /* resets funcKeyPressed then sets it to true if any function key is pressed. */
-void checkFunctionKeyPressed() {
-  funcKeyPressed = false;
+bool checkFunctionKeyPressed() {
   for (int i = 0; i < numverOfFunKeys; i++) {
     if (pressedKeys[functionKey[i]]) {
-      funcKeyPressed = true;
+      return true;
     }
   }
+  return false;
 }
 
 /* sets all the set all the pressedKeys to false. */
