83ea61fd4be4d59793523425fbc8364aa6564073
nminer <nminer@bolppaintball.com>
2016-12-30 23:13:45 -0500

    Added macro key. record and play your key strokes by saving it to EEPROM.

--- keyboard2/keyboard2.ino
+++ keyboard2/keyboard2.ino
@@ -1,3 +1,6 @@
+#include <EEPROM.h> //used for storing things in the EEPROM memorry. 
+// The Teensy 3.2 has 2048 bytes.
+
 // Array for the pins for all the comlumns.
 int Column[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 // Array for the pins for all the rows.
@@ -10,11 +13,13 @@ int keyPressDebounce = 1; // I could not set this to high or it would always rea
 int keyReleaseDebounce = 2;
 
 // All the key when function key is not pressed.
+// 'F' for function key
+// 'M' Macro key 
 int keys[] = {'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', KEY_BACKSPACE,
               KEY_TAB, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
               KEY_CAPS_LOCK, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'J', KEY_RETURN,
               KEY_LEFT_SHIFT, 'A', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'I', KEY_RIGHT_SHIFT,
-              KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, 'B', 'C', 'D', ' ', 'E', 'G', 'H', KEY_RIGHT_ALT, 'F', KEY_RIGHT_GUI, KEY_LEFT_CTRL
+              KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, ' ', ' ', ' ', ' ', ' ', ' ', ' ', KEY_RIGHT_ALT, 'F', 'M', KEY_LEFT_CTRL
              };
 
 // Array of all the keys when function key has been pressed.
@@ -22,17 +27,26 @@ int funkeys[] = {KEY_F1, KEY_F1, KEY_F2, KEY_F3, KEY_F4, KEY_F5, KEY_F6, KEY_F7,
                  KEY_TAB, 'q', KEY_UP_ARROW, 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
                  KEY_CAPS_LOCK, KEY_LEFT_ARROW, KEY_DOWN_ARROW, KEY_RIGHT_ARROW, 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', ' ', KEY_RETURN,
                  KEY_LEFT_SHIFT, ' ', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', ' ', KEY_RIGHT_SHIFT,
-                 KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, ' ', ' ', ' ', ' ', ' ', ' ', ' ', KEY_RIGHT_ALT, 'F', KEY_RIGHT_GUI, KEY_LEFT_CTRL
+                 KEY_LEFT_CTRL, KEY_LEFT_GUI, KEY_LEFT_ALT, ' ', ' ', ' ', ' ', ' ', ' ', ' ', KEY_RIGHT_ALT, 'F', 'M', KEY_LEFT_CTRL
                 };
                 
-bool pressedKeys[70];             // Keep track of what keys are p`11-22ressed.
+bool pressedKeys[70];             // Keep track of what keys are pressed.
+bool pressedFunMacroKey[70];
+bool pressedMacroKey[70];
 int pressedCount = 0;             // Keep track of how many keys are pressed.
 
 // Array that points to the function keys
 // (this should stary the same on both the keys[] and the funkeys marked with F char.
 int functionKey[] = {67};
-int numverOfFunKeys = 1; // Set to the size of the array does not like the sizeof.
+int numberOfFunKeys = 1; // Set to the size of the array does not like the sizeof.
+int macroKey[] = {68};
+int numberOfMacKeys = 1; // Set to the size of the array does not like the sizeof.
 
+bool macroRecordMode = false;     // Set to true when in macro record mode is on.
+int activeMacro = 11;             // hold the active macro being recorded.
+int tempMacro[200];               // holds the temp macro being recorded.
+int tempMacroSize = -1;            // holds the number of keys recorded.
+bool activeMacroReless = false;   // set to true when active macro num key gets released.
 /* Set all the pins for input andout put. */
 void setup() {
   //set all the columns to outpur and set them low to start.
@@ -70,16 +84,43 @@ void loop() {
 /* presses the key for the given row and column */
 void checkPressKey(int r, int c) {
   if (pressedCount < maxKeysPressed && pressedKeys[r * 14 + c] != true) {
-    if (keys[r * 14 + c] == 'F') { // if the key being pressed is a function key
+    if (keys[r * 14 + c] == 'F') { // if the key being pressed is a function key.
       Keyboard.releaseAll();
       clearAllPressed();
       pressedCount = 0;
       addKey(r, c);
+    } else if (keys[r * 14 + c] == 'M') {  // if the key being pressed is the macro key.
+      Keyboard.releaseAll();
+      clearAllPressed();
+      pressedCount = 0;
+      addKey(r, c);
+      if (macroRecordMode) {
+        endRecording();
+      }
+      macroRecordMode = false;
     } else {
       if (checkFunctionKeyPressed()) {
-        Keyboard.press(funkeys[r * 14 + c]);
+        if (!macroRecordMode && checkMacroKeyPressed() && isNumKey(funkeys[r * 14 + c])) { 
+          playMacro(keys[r * 14 + c]);
+        } else if (!macroRecordMode && checkMacroKeyPressed() && funkeys[r * 14 + c] == 'r') {
+          macroRecordMode = true;
+        } else {
+          Keyboard.press(funkeys[r * 14 + c]);
+          if (macroRecordMode) {
+            recoredKeystroke(r * 14 + c, true);
+          }
+        }
       } else {
-        Keyboard.press(keys[r * 14 + c]);
+        if (!macroRecordMode && checkMacroKeyPressed() && isNumKey(keys[r * 14 + c])) { 
+          playMacro(keys[r * 14 + c]);
+        } else if (!macroRecordMode && checkMacroKeyPressed() && keys[r * 14 + c] == 'r') {
+          macroRecordMode = true;
+        } else {
+          Keyboard.press(keys[r * 14 + c]);
+          if (macroRecordMode) {
+            recoredKeystroke(r * 14 + c, false);
+          }
+        }
       }
       addKey(r, c);
     }
@@ -93,12 +134,31 @@ void checkReleaseKey(int r, int c) {
       Keyboard.releaseAll();
       clearAllPressed();
       pressedCount = 0;
-          removeKey(r, c);
+      removeKey(r, c);
+    } else if (keys[r * 14 + c] == 'M') {
+      Keyboard.releaseAll();
+      clearAllPressed();
+      pressedCount = 0;
+      removeKey(r, c);
     } else {
       if (checkFunctionKeyPressed()) {
         Keyboard.release(funkeys[r * 14 + c]);
+        if (macroRecordMode && tempMacroSize >= 0) {
+          if (!activeMacroReless && activeMacro == funkeys[r * 14 + c]) { // ignore first release
+            activeMacroReless = false;
+          } else {
+            recoredKeystroke(r * 14 + c, true);
+          }
+        }
       } else {
         Keyboard.release(keys[r * 14 + c]);
+        if (macroRecordMode && tempMacroSize >= 0) {
+          if (!activeMacroReless && activeMacro == keys[r * 14 + c]) { // ignore first release
+            activeMacroReless = false;
+          } else {
+            recoredKeystroke(r * 14 + c, false);
+          }
+        }
       }
       removeKey(r, c);
     }
@@ -148,8 +208,18 @@ void removeKey(int r, int c) {
 
 /* resets funcKeyPressed then sets it to true if any function key is pressed. */
 bool checkFunctionKeyPressed() {
-  for (int i = 0; i < numverOfFunKeys; i++) {
+  for (int i = 0; i < numberOfFunKeys; i++) {
     if (pressedKeys[functionKey[i]]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+/* resets funcKeyPressed then sets it to true if any function key is pressed. */
+bool checkMacroKeyPressed() {
+  for (int i = 0; i < numberOfMacKeys; i++) {
+    if (pressedKeys[macroKey[i]]) {
       return true;
     }
   }
@@ -161,5 +231,133 @@ void clearAllPressed() {
   for (int i = 0; i < 70; i++) {
     pressedKeys[i] = false;
   }
+}
+
+/* clear all the macropressed keys */
+void clearAllPressedMacro() {
+  for (int i = 0; i < 70; i++) {
+    pressedFunMacroKey[i] = false;
+    pressedMacroKey[i] = false;
+  }
+}
+/* returns true if char passed in is a number from 0-9. a number key. */
+bool isNumKey(int key) {
+  return (key == '0' || key == '1' || key == '2' || key == '3' || key == '4' || key == '5' || key == '6' || key == '7' || key == '8' || key == '9');
+}
+
+/* returns and int of the char */
+int charNumToInt(int key) {
+  if (key == '0') {
+    return 0;
+  }
+  if (key == '1') {
+    return 1;
+  }
+  if (key == '2') {
+    return 2;
+  }
+  if (key == '3') {
+    return 3;
+  }
+  if (key == '4') {
+    return 4;
+  }
+  if (key == '5') {
+    return 5;
+  }
+  if (key == '6') {
+    return 6;
+  }
+  if (key == '7') {
+    return 7;
+  }
+  if (key == '8') {
+    return 8;
+  }
+  if (key == '9') {
+    return 9;
+  }
+  return 0;
+}
+
+/* recordes the keystrocks */
+void recoredKeystroke(int key, bool isfunkey) {
+  if (tempMacroSize < 0) {  // check to start the macro.
+    int mykey = keys[key];
+    if (isfunkey) {
+     mykey = funkeys[key];
+    }
+    if (isNumKey(mykey)) {  // set active macro and start recordering.
+      setActiveMacro(mykey);
+      tempMacroSize = 0;
+    } else {                // need to hit a num key befor recorde.
+      macroRecordMode = false;
+    }
+  } else {
+    if (isfunkey) {
+      key += 100;
+    }
+    tempMacro[tempMacroSize] = key;
+    tempMacroSize++;
+    if (tempMacroSize == 200) {
+      endRecording();
+    }
+  }
+}
+
+/* set the target active macro 
+  takes a int in as the macro number.
+*/
+void setActiveMacro(int mac) {
+  activeMacro = mac;
+}
+
+/* play the macro saved for the num key passed in. */
+void playMacro(int macroNum) {
+  int macN = charNumToInt(macroNum);
+  int starAdress = macN + (macN * 200);
+  int lengthOfMacro = EEPROM.read(starAdress);
+  if (lengthOfMacro <= 200) {
+    for (int i = 0; i < lengthOfMacro; i++) {
+      int tempkey = EEPROM.read(i + 1 + starAdress);
+      bool isfkey = false;
+      if (tempkey > 100) {
+        tempkey -= 100;
+        isfkey = true;
+      }
+      if (isfkey) {
+        if (pressedFunMacroKey[tempkey]) { 
+          Keyboard.release(funkeys[tempkey]);
+          pressedFunMacroKey[tempkey] = false;
+        } else {
+          Keyboard.press(funkeys[tempkey]);
+          pressedFunMacroKey[tempkey] = true;
+        }
+      } else {
+        if (pressedMacroKey[tempkey]) { 
+          Keyboard.release(keys[tempkey]);
+          pressedMacroKey[tempkey] = false;
+        } else {
+          Keyboard.press(keys[tempkey]);
+          pressedMacroKey[tempkey] = true;
+        }
+      }
+      delay(10);
+    }
+  }
+  Keyboard.releaseAll();  // just incase a key is still pressed.
+  clearAllPressedMacro();
+}
+
+/* called at the end of recording a macro. */
+void endRecording () {
+  int actMac = charNumToInt(activeMacro);
+  int starAdress = actMac + (actMac * 200);
+  EEPROM.write(starAdress, tempMacroSize);
+  for (int i = 0; i < tempMacroSize; i++) {
+    EEPROM.write(i+1+starAdress, tempMacro[i]);
+  }
+  tempMacroSize = -1; //reset the size.
+  activeMacroReless = false;
 }
 
